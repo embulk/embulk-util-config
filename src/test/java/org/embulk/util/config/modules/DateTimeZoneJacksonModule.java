a7c273883828b32a15d72f894cbccfb94ece7271
Dai MIKURUBE <dmikurube@treasure-data.com>
2020-12-30 17:19:42 +0900

    Make DateTimeZoneJacksonModule work

--- src/test/java/org/embulk/util/config/modules/DateTimeZoneJacksonModule.java
+++ src/test/java/org/embulk/util/config/modules/DateTimeZoneJacksonModule.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2014 The Embulk project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.embulk.util.config.modules;
 
 import com.fasterxml.jackson.core.JsonGenerator;
@@ -9,9 +25,13 @@ import com.fasterxml.jackson.databind.deser.std.FromStringDeserializer;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import java.io.IOException;
 
-@Deprecated
-public final class DateTimeZoneJacksonModule extends SimpleModule {
-    public DateTimeZoneJacksonModule() {
+/**
+ * Legacy SerDe of Joda-Time {@code DataTimeZone} and String in {@code embulk-core}.
+ *
+ * <p>It is copied from {@code embulk-core} v0.10.18's {@code org.embulk.spi.time.DateTimeZoneJacksonModule}, and modified a bit.
+ */
+final class DateTimeZoneJacksonModule extends SimpleModule {
+    DateTimeZoneJacksonModule() {
         this.addSerializer(org.joda.time.DateTimeZone.class, new DateTimeZoneSerializer());
         this.addDeserializer(org.joda.time.DateTimeZone.class, new DateTimeZoneDeserializer());
     }
@@ -32,7 +52,7 @@ public final class DateTimeZoneJacksonModule extends SimpleModule {
         @Override
         protected org.joda.time.DateTimeZone _deserialize(String value, DeserializationContext context)
                 throws JsonMappingException {
-            org.joda.time.DateTimeZone parsed = TimeZoneIds.parseJodaDateTimeZone(value);
+            org.joda.time.DateTimeZone parsed = JodaDateTimeZones.parseJodaDateTimeZone(value);
             if (parsed == null) {
                 // TODO include link to a document to the message for the list of supported time zones
                 throw new JsonMappingException(String.format("Unknown time zone name '%s'", value));
